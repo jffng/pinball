b50ef51283dbc1e3e82492059f0530958fbc4fbd
Jeff Ong <jonger4@gmail.com>
2014-06-17 16:36:54 -0400

    added pinball class

---
+++ js/pinball.js
@@ -0,0 +1,19 @@
+/**
+ * Pinball 
+ */
+
+var Pinball = function (_x, _y) {
+	this.fixDef = new b2FixtureDef;
+	this.fixDef.shape = new b2CircleShape( Math.random() + 0.1 )
+	this.fixDef.restitution = 1;
+
+	this.bodyDef = new b2BodyDef;
+	this.bodyDef.type = b2Body.b2_dynamicBody;	
+	this.bodyDef.position.x = _x;
+	this.bodyDef.position.y = _y;
+	world.CreateBody( this.bodyDef ).CreateFixture( this.fixDef );	
+}
+
+Pinball.prototype.draw = function() {
+	
+}
